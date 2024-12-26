08b86f3a7c20374e4c59de16ba8d13e5e6a302c6
Àlex Touza <07atouz@insernestlluch.cat>
2024-12-26 15:34:39 +0100

    Add github pages

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,13 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  prefetch: true,
-  output: 'static',
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), react()]
+	site: 'https://alex-touza.github.io',
+	base: '/fractal-explorer',
+	prefetch: true,
+	output: 'static',
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		react(),
+	],
 });
